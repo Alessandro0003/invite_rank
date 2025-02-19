c598b388b1dae5c5ab017693defec9b21f4110eb
Alessandro0003 <alessandroquinta3@gmail.com>
2025-02-18 23:19:41 -0300

    :construction: feat: change file server

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,42 @@
-import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
 import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
   validatorCompiler,
-  serializerCompiler
 } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-const app = fastify()
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
 
-app.get('/hello', () => {
-  return { hello: 'world' }
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NWL Connect API',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
 })
 
-app.listen({ port: 3333 }).then(() => {
-  console.log(`Server is running on http://localhost:3333/`)
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+
+const port = env.PORT
+
+app.listen({ port }).then(() => {
+  console.log(`Server is running on http://localhost:${port}/`)
 })
