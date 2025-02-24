7716f60f451b0118034afc9699657bbad18c37ef
Alessandro0003 <alessandroquinta3@gmail.com>
2025-02-24 20:54:59 -0300

    :sparkles: feat(routes): create subscribe-to-event route

---
+++ src/routes/subscribe-to-event.ts
@@ -0,0 +1,35 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import {
+  subscribeToEventResponseSchema,
+  subscribeToEventSchema,
+} from '../schemas/subscribe-to-event'
+import * as Service from '../services/subscribe-to-event'
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscribe to an event',
+        tags: ['Subscriptions'],
+        body: subscribeToEventSchema,
+        response: {
+          201: subscribeToEventResponseSchema,
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email, referrer } = request.body
+
+      const { subscriberId } = await Service.subscribeToEvent({
+        name,
+        email,
+        referrerId: referrer,
+      })
+
+      return reply.status(201).send({
+        subscriberId,
+      })
+    }
+  )
+}
