4fe715cf42091f481300232b97b796cd717f07ed
Alessandro0003 <alessandroquinta3@gmail.com>
2025-02-24 21:05:20 -0300

    :sparkles: feat(routes): create subscriberInvitesCount route

---
+++ src/routes/subscribe-invite-count.ts
@@ -0,0 +1,29 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import {
+  subscribeInviteCountResponse,
+  subscribeInviteCountSchema,
+} from '../schemas/subscribe-invite-count'
+import * as Services from '../services/subscriber-invites-count'
+
+export const getSubscribeInviteCountRoute: FastifyPluginAsyncZod =
+  async app => {
+    app.get(
+      '/subscribers/:subscriberId/ranking/count',
+      {
+        schema: {
+          summary: 'Get subscriber invites count',
+          tags: ['referral'],
+          params: subscribeInviteCountSchema,
+          response: {
+            200: subscribeInviteCountResponse,
+          },
+        },
+      },
+      async request => {
+        const { subscriberId } = request.params
+        const { count } = await Services.getCount({ subscriberId })
+
+        return { count }
+      }
+    )
+  }
