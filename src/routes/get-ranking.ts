78c0893d582acf1ce9d46c3951acab67e9e6ef65
Alessandro0003 <alessandroquinta3@gmail.com>
2025-02-26 22:30:32 -0300

    :sparkles: feat(routes): create getRanking route

---
+++ src/routes/get-ranking.ts
@@ -0,0 +1,23 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { rankingResponse } from '../schemas/ranking'
+import * as Service from '../services/get-ranking'
+
+export const getRanking: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/ranking',
+    {
+      schema: {
+        summary: 'Get subscriber to ranking position',
+        tags: ['referral'],
+        response: {
+          200: rankingResponse,
+        },
+      },
+    },
+    async request => {
+      const { rankingWithScore } = await Service.getRanking()
+
+      return { ranking: rankingWithScore }
+    }
+  )
+}
